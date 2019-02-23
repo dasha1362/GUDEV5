540dfda0d9445d4653b8e1a078c1a2c5c891072d
John Bell <2206568B@student.gla.ac.uk>
2019-02-23 10:52:52 +0000

    Added 2nd player character.

--- Game_Jam/Assets/Scripts/PlayerController.cs
+++ Game_Jam/Assets/Scripts/PlayerController.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    public int playerNum;
 	Rigidbody rb;
 	public float walkSpeed;
     public float grabRange;
     GameObject heldItem;
     int grabCooldown;
 
-    Animator anim;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
@@ -23,8 +24,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horInp = Input.GetAxis("Horizontal");
-        float verInp = Input.GetAxis("Vertical");
+        float horInp;
+        float verInp;
+        float grab;
+        if (playerNum == 1)
+        {
+            horInp = Input.GetAxis("HorizontalP1");
+            verInp = Input.GetAxis("VerticalP1");
+            grab = Input.GetAxis("GrabP1");
+        }
+        else
+        {
+            horInp = Input.GetAxis("HorizontalP2");
+            verInp = Input.GetAxis("VerticalP2");
+            grab = Input.GetAxis("GrabP2");
+        }
         rb.velocity = new Vector3(horInp * walkSpeed, 0f, verInp * walkSpeed);
         anim.SetFloat("hSpeed", Mathf.Abs(rb.velocity.x));
         anim.SetFloat("vSpeed", Mathf.Abs(rb.velocity.z));
@@ -46,15 +60,20 @@ public class PlayerController : MonoBehaviour
         }
         GameObject[] ingredients = GameObject.FindGameObjectsWithTag("Ingredient");
         GameObject closestIngred = UtilFunctions.FindClosestObject(transform.position, ingredients);
-        if (heldItem == null && Vector3.Distance(transform.position, closestIngred.transform.position) <= grabRange && Input.GetAxis("Grab") > 0 && grabCooldown == 0)
+        if (heldItem == null && Vector3.Distance(transform.position, closestIngred.transform.position) <= grabRange && grab > 0 && grabCooldown == 0)
         {
+            if (closestIngred.GetComponent<GrabChecker>().holdingPlayer != null)
+            {
+                closestIngred.GetComponent<GrabChecker>().holdingPlayer.GetComponent<PlayerController>().heldItem = null;
+                closestIngred.GetComponent<GrabChecker>().holdingPlayer.GetComponent<PlayerController>().anim.SetBool("holding", false);
+            }
             closestIngred.GetComponent<GrabChecker>().holdingPlayer = this.gameObject;
             closestIngred.GetComponent<GrabChecker>().isGrabbed = true;
             heldItem = closestIngred;
             grabCooldown = 30;
             anim.SetBool("holding", true);
         }
-        if (heldItem != null && Input.GetAxis("Grab") > 0 && grabCooldown == 0)
+        if (heldItem != null && grab > 0 && grabCooldown == 0)
         {
             heldItem.GetComponent<GrabChecker>().isReleased = true;
             heldItem = null;
