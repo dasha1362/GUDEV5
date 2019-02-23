57e7f59e6c00edb76801b47dbe4a643f35e45b89
John Bell <2206568B@student.gla.ac.uk>
2019-02-23 11:39:14 +0000

    Added splitscreen and fruit prefabs.

--- Game_Jam/Assets/Scripts/CameraController.cs
+++ Game_Jam/Assets/Scripts/CameraController.cs
@@ -6,12 +6,20 @@ public class CameraController : MonoBehaviour
 {
 
 	GameObject player;
-	public float followSpeed;
+    public int playerNum;
+    public float followSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindWithTag("Player");
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject p in players)
+        {
+            if (playerNum == p.GetComponent<PlayerController>().playerNum)
+            {
+                player = p;
+            }
+        }
     }
 
     // Update is called once per frame
